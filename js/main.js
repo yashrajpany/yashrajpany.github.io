6e6a9957dd9ec6a13197b889c31398f73999b4c9
Yashraj Pany <70887361+yashrajpany@users.noreply.github.com>
2021-05-12 17:49:56 +0530

    New UI changes

---
+++ js/main.js
@@ -0,0 +1,59 @@
+const about = document.querySelector('#about')
+const content = document.querySelector('#contact')
+const projects = document.getElementById('projects')
+const aboutContent = document.querySelector('#about-content')
+const contactContent = document.querySelector('#contact-content')
+const projectContent = document.querySelector('#project-content')
+
+about.addEventListener('click', () => {
+    const aboutBox = new WinBox({
+      title: 'About Me',
+    //   modal: true,
+      width: '400px',
+      height: '400px',
+      top: 10,
+      bottom: 10,
+      mount: aboutContent,
+      onfocus: function () {
+        this.setBackground('#00aa00')
+      },
+      onblur: function () {
+        this.setBackground('#777')
+      },
+    })
+  })
+  
+  contact.addEventListener('click', () => {
+    const contactBox = new WinBox({
+      title: 'Contact Me',
+      background: '#00aa00',
+      width: '400px',
+      height: '400px',
+      top: 10,
+      bottom: 10,
+      mount: contactContent,
+      onfocus: function () {
+        this.setBackground('#00aa00')
+      },
+      onblur: function () {
+        this.setBackground('#777')
+      },
+    })
+  })
+
+projects.addEventListener('click',()=>{
+    const projectBox = new WinBox({
+        title: 'Projects',
+        width: '400px',
+        height: '400px',
+        top: 10,
+        bottom: 10,
+        mount: projectContent,
+        onfocus: function () {
+            this.setBackground('#00aa00')
+          },
+        onblur: function () {
+            this.setBackground('#777')
+          },
+    })
+})
