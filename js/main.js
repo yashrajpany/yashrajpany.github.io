2420ee6c3bda74d1ccf1facaaa8ab91b4305ec52
Yashraj Pany <70887361+yashrajpany@users.noreply.github.com>
2021-05-13 10:45:51 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const about = document.querySelector('#about')
-const content = document.querySelector('#contact')
+// const content = document.querySelector('#contact')
 const projects = document.getElementById('projects')
 const aboutContent = document.querySelector('#about-content')
-const contactContent = document.querySelector('#contact-content')
+// const contactContent = document.querySelector('#contact-content')
 const projectContent = document.querySelector('#project-content')
 
 about.addEventListener('click', () => {
@@ -23,23 +23,23 @@ about.addEventListener('click', () => {
     })
   })
   
-  contact.addEventListener('click', () => {
-    const contactBox = new WinBox({
-      title: 'Contact Me',
-      background: '#00aa00',
-      width: '400px',
-      height: '400px',
-      top: 10,
-      bottom: 10,
-      mount: contactContent,
-      onfocus: function () {
-        this.setBackground('#00aa00')
-      },
-      onblur: function () {
-        this.setBackground('#777')
-      },
-    })
-  })
+//   contact.addEventListener('click', () => {
+//     const contactBox = new WinBox({
+//       title: 'Contact Me',
+//       background: '#00aa00',
+//       width: '400px',
+//       height: '400px',
+//       top: 10,
+//       bottom: 10,
+//       mount: contactContent,
+//       onfocus: function () {
+//         this.setBackground('#00aa00')
+//       },
+//       onblur: function () {
+//         this.setBackground('#777')
+//       },
+//     })
+//   })
 
 projects.addEventListener('click',()=>{
     const projectBox = new WinBox({
